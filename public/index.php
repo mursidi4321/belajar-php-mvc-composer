ea5d3f4456afe5f32a0d03f4fd5a96c205b82e3f
mursidi4321 <mursidi4321@gamail.com>
2024-03-14 16:05:27 +0700

    connecting database dengan .env

--- public/index.php
+++ public/index.php
@@ -2,13 +2,24 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\controllers\KegiatanController;
 use app\core\Application;
 use app\core\Router;
 
-$app = new Application(dirname(__DIR__));
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
